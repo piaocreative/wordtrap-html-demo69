3300335e30120775f957c07711f569d8361aaaff
piaocreative <johnpiao171019@gmail.com>
2021-10-01 20:26:48 +0800

    demo69-1001

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ $( document ).ready( function() {
     arrows: false,
     responsive: [
         {
+            breakpoint: 1650,
+            settings: {
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
             breakpoint: 992,
             settings: {
                 infinite: true,
@@ -34,6 +42,40 @@ $( document ).ready( function() {
     ]
   });
 
+  $( '.logos' ).slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+  });
+
   $( '.teams').slick({
     infinite: true,
     slidesToShow: 4,
@@ -41,6 +83,14 @@ $( document ).ready( function() {
     dots: false,
     arrows: false,
     responsive: [
+        {
+            breakpoint: 1600,
+            settings: {
+                infinite: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
         {
             breakpoint: 992,
             settings: {
